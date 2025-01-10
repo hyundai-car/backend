16bfab5abbb23112245dd19d97170e612eda2c59
Haneol <haneolj@gmail.com>
2025-01-10 17:32:22 +0900

    initial commit

---
+++ src/main/java/com/myme/mycarforme/MycarformeApplication.java
@@ -0,0 +1,13 @@
+package com.myme.mycarforme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycarformeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycarformeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myme/mycarforme/MycarformeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myme.mycarforme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycarformeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
