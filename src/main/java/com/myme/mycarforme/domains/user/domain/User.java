daae565aa4883c32c915178f8e623e7f27f244c6
Haneol <haneolj@gmail.com>
2025-01-14 11:27:11 +0900

    [feature/auth] feat: 유저 도메인 추가 (#6)

---
+++ src/main/java/com/myme/mycarforme/domains/user/domain/User.java
@@ -0,0 +1,27 @@
+package com.myme.mycarforme.domains.user.domain;
+
+import com.myme.mycarforme.global.common.jpa.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String phoneNumber;
+
+    private UserRole role;
+}
---
+++ src/main/java/com/myme/mycarforme/domains/user/domain/UserRole.java
@@ -0,0 +1,5 @@
+package com.myme.mycarforme.domains.user.domain;
+
+public enum UserRole {
+    ADMIN, USER
+}
