5f98cd571e4add1d5a4748476b7040cd1718d4ad
Haneol <haneolj@gmail.com>
2025-01-14 11:47:02 +0900

    [feature/auth] fix: 유저 도메인 수정 (#6)
    - 각 컬럼들 제약사항 추가
    - userRole 수정

--- src/main/java/com/myme/mycarforme/domains/user/domain/User.java
+++ src/main/java/com/myme/mycarforme/domains/user/domain/User.java
@@ -15,13 +15,15 @@ public class User extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String phoneNumber;
 
+    @Column(nullable = false)
     private UserRole role;
 }
--- src/main/java/com/myme/mycarforme/domains/user/domain/UserRole.java
+++ src/main/java/com/myme/mycarforme/domains/user/domain/UserRole.java
@@ -1,5 +1,25 @@
 package com.myme.mycarforme.domains.user.domain;
 
+import java.util.Arrays;
+
 public enum UserRole {
-    ADMIN, USER
+    MEMBER("member"),
+    ADMIN("admin");
+
+    private final String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public static UserRole from(String role) {
+        return Arrays.stream(values())
+                .filter(r -> r.getRole().equals(role))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Invalid role: " + role));
+    }
 }
