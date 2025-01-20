23da52039ab604aeebf73611c9ed61fbedad6f6a
Haneol <haneolj@gmail.com>
2025-01-20 21:35:11 +0900

    Update WebSocketConfig.java

--- src/main/java/com/myme/mycarforme/global/config/websocket/WebSocketConfig.java
+++ src/main/java/com/myme/mycarforme/global/config/websocket/WebSocketConfig.java
@@ -22,8 +22,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/tracking")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+                .setAllowedOriginPatterns("*");
+                // .withSockJS();
     }
 
     @Override
